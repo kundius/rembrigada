4b73b697e7c5b3670068cc74afef44f7952d7bb0
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-07-16 13:29:01 +0300

    upd

--- partials/head.php
+++ partials/head.php
@@ -1,8 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="yandex-verification" content="bf815c853d6b94b7" />
-<meta name="yandex-verification" content="be284d0d1986d0b1" />
+<meta name="yandex-verification" content="9c85ccaf464b2a6a" />
 <meta name="cmsmagazine" content="ef9002bde22164e1bb8ca1b1793cf47b" />
 <title><?php wp_title() ?></title>
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" type="text/css" />

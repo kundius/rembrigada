8af8b9b4f54ffe64864f8ae9f554fc984456e543
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-07-26 18:42:29 +0300

    deploy

--- partials/contacts-services.php
+++ partials/contacts-services.php
@@ -71,5 +71,5 @@
             </div>
         </div>
     </div>
-    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3fd55e7ae5de7ab5fde09516821ffdbb169f60b6d0a2fb8f43b6c532684a4fb4&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+    <script async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3fd55e7ae5de7ab5fde09516821ffdbb169f60b6d0a2fb8f43b6c532684a4fb4&amp;width=100%25&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
 </section>

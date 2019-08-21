dbcf81d9d5ad9b5759344ee52be3d3831a6c5eab
Kundius <kundius@localhost.localdomain>
2019-08-21 15:27:24 +0300

    deploy

--- landing.php
+++ landing.php
@@ -108,61 +108,47 @@ $reviews = new WP_Query(array(
                                     Узнайте стоимость ремонта сейчас:
                                 </div>
                                 <div class="intro-calc__layout">
-                                    <div class="intro-calc__layout-left">
-                                        <div class="intro-calc__row-object">
-                                            <div class="intro-calc__label">Что ремонтируем?</div>
-                                            <select name="object" class="js-selectys">
-                                                <option value="apartment">Квартира</option>
-                                                <option value="cottage">Коттедж</option>
-                                                <option value="office">Офис</option>
-                                                <option value="bathroom">Ванная "под ключ"</option>
-                                                <option value="kitchen">Кухня</option>
-                                            </select>
-                                        </div>
-                                        <div class="intro-calc__row-area">
-                                            <div class="intro-calc__label">Площадь <span>(м<sup>2</sup>)</span></div>
-                                            <input class="js-rangeys" type="range" name="area" min="0" max="250" value="80" />
+                                    <div class="intro-calc__row-object">
+                                        <div class="intro-calc__label">Что ремонтируем?</div>
+                                        <select name="object" class="js-selectys">
+                                            <option value="apartment">Квартира</option>
+                                            <option value="cottage">Коттедж</option>
+                                            <option value="office">Офис</option>
+                                            <option value="bathroom">Ванная "под ключ"</option>
+                                            <option value="kitchen">Кухня</option>
+                                        </select>
+                                    </div>
+                                    <div class="intro-calc__row-area">
+                                        <div class="intro-calc__label">Площадь <span>(м<sup>2</sup>)</span></div>
+                                        <input class="js-rangeys" type="range" name="area" min="0" max="250" value="80" />
+                                    </div>
+                                    <div class="intro-calc__row-type">
+                                        <div class="intro-calc__label">Вид ремонта:</div>
+                                        <div class="intro-calc__types">
+                                            <label class="intro-calc__type">
+                                                <input type="radio" name="type" value="cosmetic" checked />
+                                                <span>
+                                                    <span></span>
+                                                    Косметический
+                                                </span>
+                                            </label>
+                                            <label class="intro-calc__type">
+                                                <input type="radio" name="type" value="capital" />
+                                                <span>
+                                                    <span></span>
+                                                    Капитальный
+                                                </span>
+                                            </label>
+                                            <label class="intro-calc__type">
+                                                <input type="radio" name="type" value="european" />
+                                                <span>
+                                                    <span></span>
+                                                    Евроремонт
+                                                </span>
+                                            </label>
                                         </div>
                                     </div>
-                                    <div class="intro-calc__layout-right">
-                                        <div class="intro-calc__row-type">
-                                            <div class="intro-calc__label">Вид ремонта:</div>
-                                            <div class="intro-calc__types">
-                                                <label class="intro-calc__type">
-                                                    <input type="radio" name="type" value="cosmetic" checked />
-                                                    <span>
-                                                        <span></span>
-                                                        Косметический
-                                                    </span>
-                                                </label>
-                                                <label class="intro-calc__type">
-                                                    <input type="radio" name="type" value="capital" />
-                                                    <span>
-                                                        <span></span>
-                                                        Капитальный
-                                                    </span>
-                                                </label>
-                                                <label class="intro-calc__type">
-                                                    <input type="radio" name="type" value="european" />
-                                                    <span>
-                                                        <span></span>
-                                                        Евроремонт
-                                                    </span>
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="intro-calc__layout">
-                                    <div class="intro-calc__layout-left">
-                                        <div class="intro-calc__row-phone">
-                                            <div class="intro-calc__label-alt">Введите телефон</div>
-                                            <span class="wpcf7-form-control-wrap your-phone">
-                                                <input class="intro-calc__phone" name="your-phone" type="text" placeholder="+7" value="">
-                                            </span>
-                                        </div>
-                                    </div>
-                                    <div class="intro-calc__layout-right">
+                                    <div class="intro-calc__row-price">
                                         <div class="intro-calc__price">
                                             <div class="intro-calc__price-label">
                                                 Итого:
@@ -171,6 +157,14 @@ $reviews = new WP_Query(array(
                                                 <span class="js-intro-calc-price"></span> ₽
                                             </div>
                                         </div>
+                                    </div>
+                                    <div class="intro-calc__row-phone">
+                                        <div class="intro-calc__label-alt">Введите телефон</div>
+                                        <span class="wpcf7-form-control-wrap your-phone">
+                                            <input class="intro-calc__phone" name="your-phone" type="text" placeholder="+7" value="">
+                                        </span>
+                                    </div>
+                                    <div class="intro-calc__row-submit">
                                         <button type="submit" class="intro-calc__submit btn-arrow">
                                             Получить смету
                                         </button>

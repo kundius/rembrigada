4df5d15455beda4d2529c0859f6fcb0432c21837
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-09-23 01:26:35 +0300

    deploy

--- partials/footer.php
+++ partials/footer.php
@@ -37,93 +37,96 @@
 </footer>
 
 <div id="callback">
-    <form action="/wp-json/contact-form-7/v1/contact-forms/379/feedback" method="post" class="callback js-form">
-        <div class="callback__form">
-            <div class="callback__grid">
-                <div class="callback__cell">
-                    <div class="callback__image">
-                        <img src="<?php echo get_bloginfo('template_url') ?>/dist/img/callback.jpg" alt="" loading="lazy">
-                    </div>
-                </div>
-                <div class="callback__cell">
-                    <div class="callback-form">
-                        <div class="callback-form__title">Заказать<br> Обратный звонок</div>
-                        <div class="callback-form__desc">Заполните форму, и наш специалист свяжется с Вами в течение 15 минут</div>
-                        <div class="callback-form__fields">
-                            <div class="callback-form__row">
-                                <input type="text" name="your-name" value="" class="form-input" placeholder="Имя" />
-                            </div>
-                            <div class="callback-form__row">
-                                <span class="wpcf7-form-control-wrap your-email">
-                                    <input type="email" name="your-email" value="" class="form-input" placeholder="E-mail" />
-                                </span>
-                            </div>
-                            <div class="callback-form__row">
-                                <span class="wpcf7-form-control-wrap your-phone">
-                                    <input type="tel" name="your-phone" value="" class="form-input" placeholder="Телефон*">
-                                </span>
-                            </div>
-                            <div class="callback-form__row">
-                                <label class="callback-form__rules">
-                                    <input type="checkbox" name="rules" value="1" class="form-checkbox" />
-                                    <span></span>
-                                    Прочитал(-а) <a href="<?php the_permalink(231) ?>" target="_blank">Пользовательское соглашение</a> и соглашаюсь с <a href="<?php the_permalink(3) ?>" target="_blank">Политикой обработки персональных данных</a>
-                                </label>
-                            </div>
-                            <div class="callback-form__row">
-                                <input type="hidden" name="referrer" value="<?php the_title() ?>">
-                                <button type="submit" class="form-submit"><span></span><span>Отправить</span></button>
-                            </div>
+    <div class="modal modal_callback">
+        <button class="modal__close"></button>
+        <form action="/wp-json/contact-form-7/v1/contact-forms/379/feedback" method="post" class="callback js-form">
+            <div class="callback__form">
+                <div class="callback__grid">
+                    <div class="callback__cell">
+                        <div class="callback__image">
+                            <img src="<?php echo get_bloginfo('template_url') ?>/dist/img/callback.jpg" alt="" loading="lazy">
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="callback__success">
-            <div class="callback__grid">
-                <div class="callback__cell">
-                    <div class="callback__image">
-                        <img src="<?php echo get_bloginfo('template_url') ?>/dist/img/our-work-send.jpg" alt="" loading="lazy">
-                        <a href="<?php the_permalink(15) ?>" class="callback__works">
-                            <span>Наши работы</span>
-                        </a>
-                    </div>
-                </div>
-                <div class="callback__cell">
-                    <div class="callback-form">
-                        <div class="callback-form__title">Ваша заявка успешно отправлена</div>
-                        <div class="callback-form__desc">Наш специалист свяжется с Вами в течении 15 минут, а пока вы можете ознакомиться с видами ремонта, которые мы делаем и как мы его делаем.</div>
-                        <div class="callback-items">
-                            <div class="callback-item">
-                                <div class="callback-item__image">
-                                    <div><img src="<?php echo get_bloginfo('template_url') ?>/dist/img/apartments.svg" alt="" loading="lazy"></div>
+                    <div class="callback__cell">
+                        <div class="callback-form">
+                            <div class="callback-form__title">Заказать<br> Обратный звонок</div>
+                            <div class="callback-form__desc">Заполните форму, и наш специалист свяжется с Вами в течение 15 минут</div>
+                            <div class="callback-form__fields">
+                                <div class="callback-form__row">
+                                    <input type="text" name="your-name" value="" class="form-input" placeholder="Имя" />
                                 </div>
-                                <div class="callback-item__title">
-                                    Ремонт квартир
+                                <div class="callback-form__row">
+                                    <span class="wpcf7-form-control-wrap your-email">
+                                        <input type="email" name="your-email" value="" class="form-input" placeholder="E-mail" />
+                                    </span>
                                 </div>
-                            </div>
-                            <div class="callback-item">
-                                <div class="callback-item__image">
-                                    <div><img src="<?php echo get_bloginfo('template_url') ?>/dist/img/houses.svg" alt="" loading="lazy"></div>
+                                <div class="callback-form__row">
+                                    <span class="wpcf7-form-control-wrap your-phone">
+                                        <input type="tel" name="your-phone" value="" class="form-input" placeholder="Телефон*">
+                                    </span>
                                 </div>
-                                <div class="callback-item__title">
-                                    Ремонт домов
+                                <div class="callback-form__row">
+                                    <label class="callback-form__rules">
+                                        <input type="checkbox" name="rules" value="1" class="form-checkbox" />
+                                        <span></span>
+                                        Прочитал(-а) <a href="<?php the_permalink(231) ?>" target="_blank">Пользовательское соглашение</a> и соглашаюсь с <a href="<?php the_permalink(3) ?>" target="_blank">Политикой обработки персональных данных</a>
+                                    </label>
                                 </div>
-                            </div>
-                            <div class="callback-item">
-                                <div class="callback-item__image">
-                                    <div><img src="<?php echo get_bloginfo('template_url') ?>/dist/img/commercial.svg" alt="" loading="lazy"></div>
-                                </div>
-                                <div class="callback-item__title">
-                                    Ремонт коммерческой недвижимости
+                                <div class="callback-form__row">
+                                    <input type="hidden" name="referrer" value="<?php the_title() ?>">
+                                    <button type="submit" class="form-submit"><span></span><span>Отправить</span></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
+            <div class="callback__success">
+                <div class="callback__grid">
+                    <div class="callback__cell">
+                        <div class="callback__image">
+                            <img src="<?php echo get_bloginfo('template_url') ?>/dist/img/our-work-send.jpg" alt="" loading="lazy">
+                            <a href="<?php the_permalink(15) ?>" class="callback__works">
+                                <span>Наши работы</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="callback__cell">
+                        <div class="callback-form">
+                            <div class="callback-form__title">Ваша заявка успешно отправлена</div>
+                            <div class="callback-form__desc">Наш специалист свяжется с Вами в течении 15 минут, а пока вы можете ознакомиться с видами ремонта, которые мы делаем и как мы его делаем.</div>
+                            <div class="callback-items">
+                                <div class="callback-item">
+                                    <div class="callback-item__image">
+                                        <div><img src="<?php echo get_bloginfo('template_url') ?>/dist/img/apartments.svg" alt="" loading="lazy"></div>
+                                    </div>
+                                    <div class="callback-item__title">
+                                        Ремонт квартир
+                                    </div>
+                                </div>
+                                <div class="callback-item">
+                                    <div class="callback-item__image">
+                                        <div><img src="<?php echo get_bloginfo('template_url') ?>/dist/img/houses.svg" alt="" loading="lazy"></div>
+                                    </div>
+                                    <div class="callback-item__title">
+                                        Ремонт домов
+                                    </div>
+                                </div>
+                                <div class="callback-item">
+                                    <div class="callback-item__image">
+                                        <div><img src="<?php echo get_bloginfo('template_url') ?>/dist/img/commercial.svg" alt="" loading="lazy"></div>
+                                    </div>
+                                    <div class="callback-item__title">
+                                        Ремонт коммерческой недвижимости
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 
 <button class="scrollup js-scroll"></button>

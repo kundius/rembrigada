a471e7ba25b93f8079f02ceef6ddc6023071dd89
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-09-22 22:58:38 +0300

    deploy

--- single-project.php
+++ single-project.php
@@ -127,8 +127,8 @@ function get_next_id($list, $id) {
                                                     <div class="project-modal-layout__right">
                                                         <div class="project-modal__nav">
                                                             <div class="slide-nav">
-                                                                <button class="slide-nav__button slide-nav__button_prev js-project-modal-prev" data-target="project-<?php echo get_prev_id($gallery, $item['id']) ?>"></button>
-                                                                <button class="slide-nav__button slide-nav__button_next js-project-modal-next" data-target="project-<?php echo get_next_id($gallery, $item['id']) ?>"></button>
+                                                                <div class="slide-nav__button slide-nav__button_prev js-project-modal-prev" data-target="project-<?php echo get_prev_id($gallery, $item['id']) ?>"></div>
+                                                                <div class="slide-nav__button slide-nav__button_next js-project-modal-next" data-target="project-<?php echo get_next_id($gallery, $item['id']) ?>"></div>
                                                             </div>
                                                         </div>
                                                         <div>

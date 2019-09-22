53e9f5c436bfa695f46fd29a0f5ae36ebdd7f34d
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-09-22 23:03:24 +0300

    deploy

--- single-project.php
+++ single-project.php
@@ -126,7 +126,7 @@ function get_next_id($list, $id) {
                                                     </div>
                                                     <div class="project-modal-layout__right">
                                                         <div class="project-modal__nav">
-                                                            <div class="slide-nav">
+                                                            <div class="slide-nav" data-micromodal-close>
                                                                 <button class="slide-nav__button slide-nav__button_prev" data-micromodal-trigger="project-<?php echo get_prev_id($gallery, $item['id']) ?>"></button>
                                                                 <button class="slide-nav__button slide-nav__button_next" data-micromodal-trigger="project-<?php echo get_next_id($gallery, $item['id']) ?>"></button>
                                                             </div>

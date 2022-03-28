8d0029db377343ffe58bbba77b6d1c28e2d36743
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-03-28 23:48:17 +0300

    deploy

--- index.php
+++ index.php
@@ -44,19 +44,12 @@
                 </div>
             </div>
 
-            <div class="services-content">
-                <div class="container container_tiny">
-                    <div class="content">
-                        <?php the_content() ?>
-                    </div>
-                </div>
-            </div>
+            <?php the_content() ?>
+
             <?php endwhile; else: ?>
                 <p>Извините, ничего не найдено.</p>
             <?php endif; ?>
 
-            <?php if (get_field('show_scheme')): get_template_part('partials/scheme'); endif; ?>
-            <?php if (get_field('show_contacts')): get_template_part('partials/contacts', 'services'); endif; ?>
             <?php get_template_part('partials/footer') ?>
         </div>
     </body>

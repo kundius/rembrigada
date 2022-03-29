3649a1668258ad4dcabeb728aa393b2b736b3efa
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-03-29 03:41:05 +0300

    deploy

--- partials/content-services.php
+++ partials/content-services.php
@@ -1,35 +1,21 @@
-<?php
-$services = new WP_Query(array(
-  'post_type' => 'page',
-  // 'post_parent' => 11,
-  'order' => 'ASC',
-  'orderby' => 'menu_order',
-  'meta_query'	=> array(
-    array(
-      'key'	 	=> 'show_at_home',
-      'value'	  	=> '1',
-      'compare' 	=> '=',
-    )
-  )
-));
-?>
-
-<?php if ($services->have_posts()): ?>
+<?php $services = get_field('services', 'option'); ?>
 <section class="type-of-repair">
-    <?php while($services->have_posts()): $services->the_post(); ?>
+  <div class="container">
+    <div class="type-of-repair__title"><?php echo $services['title'] ?></div>
+    <?php foreach ($services['items'] as $service): ?>
     <div class="type-of-repair__cell">
       <div class="type-of-repair-item">
-          <div class="type-of-repair__title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></div>
+          <div class="type-of-repair__title"><?php echo $service['name'] ?></div>
           <div class="type-of-repair-item__inner">
               <div class="repair-img-label">
                   <a href="<?php the_permalink() ?>">
-                      <?php if ($image = get_the_post_thumbnail_url(get_the_ID(), array(468, 364))): ?>
-                      <img class="repair-img" src="<?php echo $image ?>" alt="<?php the_title() ?>" loading="lazy">
+                      <?php if ($image = $service['image']): ?>
+                      <img class="repair-img" src="<?php echo $image ?>" alt="" loading="lazy">
                       <?php else: ?>
                       <img class="repair-img" src="https://via.placeholder.com/468x364" alt="" loading="lazy">
                       <?php endif; ?>
                   </a>
-                  <?php if ($price = get_field('price', get_the_ID())): ?>
+                  <?php if ($price = $service['price']): ?>
                   <div class="label">
                       <p>
                           <?php echo $price['prefix'] ?>
@@ -42,6 +28,6 @@ $services = new WP_Query(array(
           </div>
       </div>
     </div>
-    <?php endwhile; ?>
+    <?php endforeach; ?>
+  </div>
 </section>
-<?php endif; wp_reset_query(); ?>

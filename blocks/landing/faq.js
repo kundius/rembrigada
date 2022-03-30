50ca3875c0aaf33c702962d011601434a54e1ad1
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-03-30 18:28:31 +0300

    deploy

---
+++ blocks/landing/faq.js
@@ -0,0 +1,32 @@
+( function( blocks, element ) {
+
+	const el = element.createElement;
+	const { registerBlockType } = blocks;
+
+	registerBlockType( 'landing/faq', {
+		title: 'Ответы на вопросы',
+		icon: 'embed-generic',
+		category: 'widgets',
+		keywords: [ 'faq' ],
+
+		// The "edit" property must be a valid function.
+		edit: function( props ) {
+			return el(
+				'div',
+				{ className: props.className },
+				'Ответы на вопросы'
+			);
+		},
+
+		save: function(props) {
+			return el(
+				'div',
+				{ className: props.className },
+				'[template_part path="partials/landing/faq"]'
+			);
+		}
+	});
+})(
+	window.wp.blocks,
+	window.wp.element
+);
---
+++ partials/landing/faq.php
@@ -0,0 +1,36 @@
+<?php $faq = get_field('faq', 'option'); ?>
+<section class="landing-faq">
+  <div class="container container_medium">
+    <?php if (!empty($faq['title'])): ?>
+    <div class="landing-faq__title"><?php echo $faq['title'] ?></div>
+    <?php endif; ?>
+
+    <div class="landing-faq__list">
+      <?php foreach ($faq['items'] as $item): ?>
+      <div class="faq-item">
+        <div class="faq-item__question">
+          <div class="faq-item__question-icon"></div>
+          <div class="faq-item__question-content">
+            <?php echo $service['question'] ?>
+          </div>
+        </div>
+        <div class="faq-item__answer">
+          <div class="faq-item__answer-content">
+            <?php echo $service['answer'] ?>
+          </div>
+        </div>
+      </div>
+      <?php endforeach; ?>
+    </div>
+
+    <?php if (!empty($faq['description'])): ?>
+    <div class="landing-faq__description"><?php echo $faq['description'] ?></div>
+    <?php endif; ?>
+
+    <?php if (!empty($faq['request'])): ?>
+    <div class="landing-faq__request">
+      <button class="landing-button"><?php echo $faq['request'] ?></button>
+    </div>
+    <?php endif; ?>
+  </div>
+</section>

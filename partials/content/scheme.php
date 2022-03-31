2854e5509ea27ba4f3e0728e7f69208cd31ecea2
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-03-31 21:20:27 +0300

    deploy

--- partials/content/scheme.php
+++ partials/content/scheme.php
@@ -1,20 +1,18 @@
 <?php if ($scheme = get_field('scheme', 'option')): ?>
 <div class="content-scheme">
   <?php foreach ($scheme['items'] as $item): ?>
-  <div class="content-scheme__cell">
-    <div class="content-scheme-item">
-      <?php if ($item['image']): ?>
-      <div class="content-scheme-item__image">
-        <img src="<?php echo $item['image']['url'] ?>" alt="">
-      </div>
-      <?php endif; ?>
-
-      <?php if ($item['title']): ?>
-      <div class="content-scheme-item__title">
-        <?php echo $item['title'] ?>
-      </div>
-      <?php endif; ?>
+  <div class="content-scheme-item">
+    <?php if ($item['image']): ?>
+    <div class="content-scheme-item__image">
+      <img src="<?php echo $item['image']['url'] ?>" alt="">
     </div>
+    <?php endif; ?>
+
+    <?php if ($item['title']): ?>
+    <div class="content-scheme-item__title">
+      <?php echo $item['title'] ?>
+    </div>
+    <?php endif; ?>
   </div>
   <?php endforeach; ?>
 </div>

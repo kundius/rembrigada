2b057b48bfda90c4ceff2f5fc85da31dd596cef3
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-01 01:36:25 +0300

    deploy

--- partials/blocks/content-portfolio.php
+++ partials/blocks/content-portfolio.php
@@ -1,3 +1,76 @@
-<?php if ($list = get_field('content-portfolio')): print_r($list); ?>
-
+<?php if ($list = get_field('content-portfolio')): ?>
+<div class="wp-block-content-portfolio">
+  <?php foreach($list as $item): ?>
+  <div class="works-item">
+      <div class="works-item__title"><?php the_title($item->ID) ?></div>
+      <div class="works-item__grid">
+          <div class="works-item__cell">
+              <?php if ($gallery = get_field('gallery', $item->ID)): ?>
+              <div class="works-item-images<?php if (count($gallery) == 1): ?> works-item-images_single<?php endif; ?>">
+                  <?php foreach (array_splice($gallery, 0, 8) as $key => $item): ?>
+                  <div class="works-item-image">
+                      <a href="<?php echo $item['sizes']['large'] ?>" data-fslightbox="project-<?php echo $item->ID ?>" class="works-item-image__wrapper">
+                          <span class="works-item-image__inner" style="background-image: url('<?php echo $item['sizes'][$key == 0 ? 'w800h600' : 'w150h100'] ?>')"></span>
+                          <span class="works-item-image__loupe"><?php icon('loupe') ?></span>
+                      </a>
+                  </div>
+                  <?php endforeach; ?>
+              </div>
+              <?php endif; ?>
+          </div>
+          <div class="works-item__cell">
+              <?php if ($address = get_field('address', $item->ID)): ?>
+              <div class="works-item-object">
+                  <div class="works-item-object__label">
+                      Объект:
+                  </div>
+                  <div class="works-item-object__value">
+                      <?php echo $address ?>
+                  </div>
+              </div>
+              <?php endif; ?>
+              <div class="works-item-info">
+                  <!-- <div class="works-item-info__title">
+                      Особенности проекта:
+                  </div> -->
+                  <?php if ($area = get_field('area', $item->ID)): ?>
+                  <div class="works-item-info__row">
+                      <div class="works-item-info__label">
+                          Площадь:
+                      </div>
+                      <div class="works-item-info__area">
+                          <?php echo $area ?> <span>м<sup>2</sup></span>
+                      </div>
+                  </div>
+                  <?php endif; ?>
+              </div>
+              <?php if ($time_works = get_field('time_works', $item->ID)): ?>
+              <div class="works-item-deadline">
+                  <div class="works-item-deadline__label">
+                      Сроки:
+                  </div>
+                  <div class="works-item-deadline__value">
+                      <?php echo $time_works ?>
+                  </div>
+              </div>
+              <?php endif; ?>
+              <div class="works-item-pricing">
+                  <?php if (get_field('price_works', $item->ID) || get_field('price_material', $item->ID)): ?>
+                      <div class="works-item-pricing__title">Стоимость:</div>
+                  <?php endif; ?>
+                  <div class="works-item-pricing__text">
+                      <?php if ($price_works = get_field('price_works', $item->ID)): ?>
+                      <div>Ремонтные работы: <strong><?php echo $price_works ?> руб.</strong></div>
+                      <?php endif; ?>
+                      <?php if ($price_material = get_field('price_material', $item->ID)): ?>
+                      <div>Черновые материалы с доставкой: <strong><?php echo $price_material ?> руб.</strong></div>
+                      <?php endif; ?>
+                  </div>
+                  <a href="#callback" class="works-item-pricing__button"><span>Рассчитать стоимость своего ремонта</span></a>
+              </div>
+          </div>
+      </div>
+  </div>
+  <?php endforeach; ?>
+</div>
 <?php endif; ?>

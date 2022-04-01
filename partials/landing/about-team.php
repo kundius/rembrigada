7e2cffd6e580592b6efe54f0fdad32c5b458eb2e
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-01 03:05:08 +0300

    deploy

--- partials/landing/about-team.php
+++ partials/landing/about-team.php
@@ -4,20 +4,22 @@
     <?php if (!empty($about_team['title'])): ?>
     <div class="landing-about-team__title"><?php echo $about_team['title'] ?></div>
     <?php endif; ?>
-    <?php foreach ($about_team['items'] as $item): ?>
-    <div class="about-team-item">
-      <div class="about-team-item__body">
-        <div class="about-team-item__title"><?php echo $item['title'] ?></div>
-        <div class="about-team-item__description"><?php echo $item['description'] ?></div>
+    <div class="landing-about-team__items">
+      <?php foreach ($about_team['items'] as $item): ?>
+      <div class="about-team-item">
+        <div class="about-team-item__body">
+          <div class="about-team-item__title"><?php echo $item['title'] ?></div>
+          <div class="about-team-item__description"><?php echo $item['description'] ?></div>
+        </div>
+        <div class="about-team-item__image">
+          <?php if ($image = $item['image']): ?>
+          <img src="<?php echo $image['url'] ?>" alt="" loading="lazy">
+          <?php else: ?>
+          <img src="https://via.placeholder.com/468x364" alt="" loading="lazy">
+          <?php endif; ?>
+        </div>
       </div>
-      <div class="about-team-item__image">
-        <?php if ($image = $item['image']): ?>
-        <img src="<?php echo $image['url'] ?>" alt="" loading="lazy">
-        <?php else: ?>
-        <img src="https://via.placeholder.com/468x364" alt="" loading="lazy">
-        <?php endif; ?>
-      </div>
+      <?php endforeach; ?>
     </div>
-    <?php endforeach; ?>
   </div>
 </section>

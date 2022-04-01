e0e1262aa2b11f2e0cbdb4744ad21f0f0d92b388
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-01 03:02:43 +0300

    deploy

--- partials/landing/about-team.php
+++ partials/landing/about-team.php
@@ -4,8 +4,20 @@
     <?php if (!empty($about_team['title'])): ?>
     <div class="landing-about-team__title"><?php echo $about_team['title'] ?></div>
     <?php endif; ?>
-    <?php if (!empty($about_team['content'])): ?>
-    <div class="landing-about-team__content"><?php echo $about_team['content'] ?></div>
-    <?php endif; ?>
+    <?php foreach ($about_team['items'] as $item): ?>
+    <div class="about-team-item">
+      <div class="about-team-item__body">
+        <div class="about-team-item__title"><?php echo $item['title'] ?></div>
+        <div class="about-team-item__description"><?php echo $item['description'] ?></div>
+      </div>
+      <div class="about-team-item__image">
+        <?php if ($image = $item['image']): ?>
+        <img src="<?php echo $image['url'] ?>" alt="" loading="lazy">
+        <?php else: ?>
+        <img src="https://via.placeholder.com/468x364" alt="" loading="lazy">
+        <?php endif; ?>
+      </div>
+    </div>
+    <?php endforeach; ?>
   </div>
 </section>

ee8402f0a2df66a186b0a33aaf0494e178de13c6
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-02 04:22:45 +0300

    deploy

--- partials/landing/quiz.php
+++ partials/landing/quiz.php
@@ -5,7 +5,7 @@
     </div>
     <div class="quiz">
       <div class="quiz-steps">
-        <div class="quiz-steps__item">1</div>
+        <div class="quiz-steps__item quiz-steps__item_active">1</div>
         <div class="quiz-steps__line"></div>
         <div class="quiz-steps__item">2</div>
         <div class="quiz-steps__line"></div>
@@ -24,55 +24,55 @@
       <div class="quiz-layout">
         <div class="quiz-layout__screens">
           <div class="quiz-screens">
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item quiz-screens__item_active">
               1
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               2
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               3
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               4
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               5
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               6
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               7
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>
                 <button class="quiz-buttons__item quiz-screens__next">Следующий шаг</button>
               </div>
             </div>
-            <div class="quiz-screens__item" data-quiz-screen>
+            <div class="quiz-screens__item">
               8
               <div class="quiz-buttons">
                 <button class="quiz-buttons__item quiz-screens__previous">Назад</button>

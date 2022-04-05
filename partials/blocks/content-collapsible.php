052335cec43b71843485e1ffe9212da78dd78762
Kundius Ruslan <kundius.ruslan@gmail.com>
2022-04-05 18:39:36 +0300

    deploy

--- partials/blocks/content-collapsible.php
+++ partials/blocks/content-collapsible.php
@@ -1,5 +1,10 @@
 <?php if ($text = get_field('content-collapsible')): ?>
-<div class="wp-block-content-collapsible">
-    <?php echo $text ?>
+<div class="content-collapsible">
+    <div class="content-collapsible__body">
+        <div class="content-collapsible__inner">
+            <?php echo $text ?>
+        </div>
+    </div>
+    <button class="content-collapsible__button"></button>
 </div>
 <?php endif; ?>

ff06b0d3148f4f8b5a1fe7def5b0e96d7651fa2d
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-01-11 10:41:10 +0000

    deploy

--- partials/content/masters.php
+++ partials/content/masters.php
@@ -1,28 +1,26 @@
 <?php $data = get_field('masters', 'option'); ?>
-<section class="content-masters">
-  <div class="container">
-    <?php if (!empty($data['title'])): ?>
-    <div class="content-masters__title"><?php echo $data['title'] ?></div>
-    <?php endif; ?>
-    <div class="content-masters__grid">
-      <?php foreach ($data['items'] as $item): ?>
-      <div class="content-masters__cell">
-        <div class="content-masters-item">
-          <div class="content-masters-item__image">
-            <?php if ($image = $item['image']): ?>
-            <img src="<?php echo $image['sizes']['w468h364'] ?>" alt="" loading="lazy">
-            <?php else: ?>
-            <img src="https://via.placeholder.com/468x364" alt="" loading="lazy">
-            <?php endif; ?>
-          </div>
-          <div class="content-masters-item__inner">
-            <div class="content-masters-item__name"><?php echo $item['name'] ?></div>
-            <div class="content-masters-item__specialization"><?php echo $item['specialization'] ?></div>
-            <div class="content-masters-item__experience"><?php echo $item['experience'] ?></div>
-          </div>
+<div class="content-masters">
+  <?php if (!empty($data['title'])): ?>
+  <div class="content-masters__title"><?php echo $data['title'] ?></div>
+  <?php endif; ?>
+  <div class="content-masters__grid">
+    <?php foreach ($data['items'] as $item): ?>
+    <div class="content-masters__cell">
+      <div class="content-masters-item">
+        <div class="content-masters-item__image">
+          <?php if ($image = $item['image']): ?>
+          <img src="<?php echo $image['sizes']['w468h364'] ?>" alt="" loading="lazy">
+          <?php else: ?>
+          <img src="https://via.placeholder.com/468x364" alt="" loading="lazy">
+          <?php endif; ?>
+        </div>
+        <div class="content-masters-item__inner">
+          <div class="content-masters-item__name"><?php echo $item['name'] ?></div>
+          <div class="content-masters-item__specialization"><?php echo $item['specialization'] ?></div>
+          <div class="content-masters-item__experience"><?php echo $item['experience'] ?></div>
         </div>
       </div>
-      <?php endforeach; ?>
     </div>
+    <?php endforeach; ?>
   </div>
-</section>
+</div>

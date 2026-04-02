d56d03961f1529760e2c9dc487a5cd9151d5582a
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-02 16:11:41 +0300

    deploy

--- partials/conversation.php
+++ partials/conversation.php
@@ -1,131 +1,117 @@
-<div class="conversion conversion_widget">
-  <div class="conversion-container conversion_widget-container">
-    <div class="conversion_widget-content">
-      <div class="conversion_widget-content-inner">
-        <div class="beauty-scroller conversion_widget-content-scroller">
-          <div class="conversion_widget-content-container">
-            <div class="conversion_widget-content-container-inner">
-              <div class="conversion_widget-row row">
-                
+<div class="conversion conversion--widget">
+  
+  <!-- Контейнер виджета -->
+  <div class="conversion__container">
+    <div class="conversion__content">
+      <div class="conversion__content-inner">
+        <div class="conversion__content-scroller">
+          <div class="conversion__content-container">
+            <div class="conversion__content-container-inner">
+              <div class="conversion__row row">
+
                 <!-- Калькулятор дизайна -->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 conversion_widget-item">
-                  <div class="conversion_widget-item-inner">
-                    <div class="conversion_widget-item-title">Калькулятор дизайна</div>
-                    <div class="conversion_widget-item-content">
-                      <span>
-                        <a class="calculator-design call-wqec conversion_widget-item-btn conversion_widget-item-calculator-design" 
-                           data-wqec-section-id="4927" 
-                           href="#">
-                          <i class="fa fa-calculator-design"></i>
-                        </a>
-                      </span>
+                <div class="conversion__item col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                  <div class="conversion__item-inner">
+                    <div class="conversion__item-title">Калькулятор дизайна</div>
+                    <div class="conversion__item-content">
+                      <a class="conversion__item-btn conversion__item-btn--calculator-design call-wqec" 
+                         data-wqec-section-id="4927" 
+                         href="#">
+                        <i class="fa fa-calculator-design"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
 
                 <!-- Калькулятор ремонта -->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 conversion_widget-item">
-                  <div class="conversion_widget-item-inner">
-                    <div class="conversion_widget-item-title">Калькулятор ремонта</div>
-                    <div class="conversion_widget-item-content">
-                      <span>
-                        <a class="calculator-renovation call-wqec conversion_widget-item-btn conversion_widget-item-calculator-renovation" 
-                           data-wqec-section-id="4928" 
-                           href="#">
-                          <i class="fa fa-calculator-renovation"></i>
-                        </a>
-                      </span>
+                <div class="conversion__item col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                  <div class="conversion__item-inner">
+                    <div class="conversion__item-title">Калькулятор ремонта</div>
+                    <div class="conversion__item-content">
+                      <a class="conversion__item-btn conversion__item-btn--calculator-renovation call-wqec" 
+                         data-wqec-section-id="4928" 
+                         href="#">
+                        <i class="fa fa-calculator-renovation"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
 
-                <!-- Позвонить нам -->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 conversion_widget-item">
-                  <div class="conversion_widget-item-inner">
-                    <div class="conversion_widget-item-title">
+                <!-- Позвонить -->
+                <div class="conversion__item col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                  <div class="conversion__item-inner">
+                    <div class="conversion__item-title">
                       Позвонить нам 
                       <span>Ежедневно <br>9:00–20:00</span>
                     </div>
-                    <div class="conversion_widget-item-content">
-                      <span>
-                        <a class="conversion_widget-item-btn conversion_widget-item-phone-call phone-call" 
-                           href="tel:+74953571167" 
-                           title="Позвонить нам">
-                          <i class="fa fa-phone-call"></i>
-                        </a>
-                      </span>
+                    <div class="conversion__item-content">
+                      <a class="conversion__item-btn conversion__item-btn--phone-call phone-call" 
+                         href="tel:+74953571167" 
+                         title="Позвонить нам">
+                        <i class="fa fa-phone-call"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
 
                 <!-- Обратный звонок -->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 conversion_widget-item">
-                  <div class="conversion_widget-item-inner">
-                    <div class="conversion_widget-item-title">Заказать обратный <br>звонок</div>
-                    <div class="conversion_widget-item-content">
-                      <span>
-                        <a class="b24-web-form-popup-btn-45 btn-loader conversion_widget-item-btn conversion_widget-item-phone-back phone-back" 
-                           data-load="click" 
-                           href="/contacts/" 
-                           title="Обратный звонок">
-                          <i class="fa fa-phone-back"></i>
-                        </a>
-                      </span>
+                <div class="conversion__item col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                  <div class="conversion__item-inner">
+                    <div class="conversion__item-title">Заказать обратный <br>звонок</div>
+                    <div class="conversion__item-content">
+                      <a class="conversion__item-btn conversion__item-btn--phone-back b24-web-form-popup-btn-45 btn-loader" 
+                         data-load="click" 
+                         href="/contacts/" 
+                         title="Обратный звонок">
+                        <i class="fa fa-phone-back"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
 
-                <!-- Написать нам -->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 conversion_widget-item">
-                  <div class="conversion_widget-item-inner">
-                    <div class="conversion_widget-item-title">
+                <!-- Написать -->
+                <div class="conversion__item col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                  <div class="conversion__item-inner">
+                    <div class="conversion__item-title">
                       Написать нам 
                       <span>Email и Обратная связь</span>
                     </div>
-                    <div class="conversion_widget-item-content">
-                      <span class="ct_phone_vira_2_2024">
-                        <a href="mailto:vira@eremont.ru?subject=Обращение с сайта eremont.ru.+7 (495) 357-11-67" 
-                           class="conversion_widget-item-btn conversion_widget-item-email email" 
-                           title="Написать нам">
-                          <i class="fa fa-email"></i>
-                        </a>
-                      </span>
-                      <span>
-                        <a class="b24-web-form-popup-btn-27 btn-loader conversion_widget-item-btn conversion_widget-item-form form-back" 
-                           data-load="click" 
-                           href="/contacts/" 
-                           title="Обратная связь">
-                          <i class="fa fa-form"></i>
-                        </a>
-                      </span>
+                    <div class="conversion__item-content">
+                      <a href="mailto:vira@eremont.ru?subject=Обращение с сайта eremont.ru.+7 (495) 357-11-67" 
+                         class="conversion__item-btn conversion__item-btn--email" 
+                         title="Написать нам">
+                        <i class="fa fa-email"></i>
+                      </a>
+                      <a class="conversion__item-btn conversion__item-btn--form b24-web-form-popup-btn-27 btn-loader" 
+                         data-load="click" 
+                         href="/contacts/" 
+                         title="Обратная связь">
+                        <i class="fa fa-form"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
 
-                <!-- Чат (Телеграм / WhatsApp) -->
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 conversion_widget-item">
-                  <div class="conversion_widget-item-inner">
-                    <div class="conversion_widget-item-title">
+                <!-- Чат -->
+                <div class="conversion__item col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                  <div class="conversion__item-inner">
+                    <div class="conversion__item-title">
                       Начать чат 
                       <span>Телеграм и WhatsApp</span>
                     </div>
-                    <div class="conversion_widget-item-content">
-                      <span class="ct_phone_vira_1_2024">
-                        <a href="https://t.me/viraartstroy?text=Здравствуйте! Прошу проконсультировать меня касательно ваших услуг. (телефон для консультаций +7 (495) 357-11-67)" 
-                           class="conversion_widget-item-btn conversion_widget-item-telegram telegram" 
-                           target="_blank" 
-                           title="Написать в Телеграм">
-                          <i class="fa fa-telegram"></i>
-                        </a>
-                      </span>
-                      <span class="ct_phone_vira_3_2024">
-                        <a href="https://wa.me/74953571488?text=Здравствуйте! Прошу проконсультировать меня касательно ваших услуг. (телефон для консультаций +7 (495) 357-11-67)" 
-                           class="conversion_widget-item-btn conversion_widget-item-whatsapp whatsapp" 
-                           target="_blank" 
-                           title="Написать в whatsApp">
-                          <i class="fa fa-whatsapp"></i>
-                        </a>
-                      </span>
+                    <div class="conversion__item-content">
+                      <a href="https://t.me/viraartstroy?text=Здравствуйте!..." 
+                         class="conversion__item-btn conversion__item-btn--telegram" 
+                         target="_blank" 
+                         title="Написать в Телеграм">
+                        <i class="fa fa-telegram"></i>
+                      </a>
+                      <a href="https://wa.me/74953571488?text=Здравствуйте!..." 
+                         class="conversion__item-btn conversion__item-btn--whatsapp" 
+                         target="_blank" 
+                         title="Написать в WhatsApp">
+                        <i class="fa fa-whatsapp"></i>
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -135,31 +121,24 @@
           </div>
         </div>
       </div>
-      <div class="conversion_widget-content-backdrop"></div>
+      <div class="conversion__content-backdrop"></div>
     </div>
-    <div class="conversion_widget-backdrop"></div>
+    <div class="conversion__backdrop"></div>
   </div>
 
   <!-- Кнопка-триггер -->
-  <div class="conversion-btn conversion_widget-btn green">
-    <div class="conversion_widget-btn-wave"></div>
-    <a class="conversion-toggler conversion_widget-toggler" href="#">
-      <div class="play" style="display: block;">
-        <span data-color="blue" style="display: none;">
-          <i class="fa fa-phone-call"></i>
-        </span>
-        <span class="whatsapp" data-color="green" style="display: block;">
-          <i class="fa fa-whatsapp"></i>
-        </span>
-        <span class="email" data-color="blue" style="display: none;">
-          <i class="fa fa-email"></i>
-        </span>
+  <div class="conversion__btn conversion__btn--gray">
+    <div class="conversion__btn-wave"></div>
+    <button class="conversion__toggler" type="button">
+      <div class="conversion__play">
+        <span class="conversion__icon" data-color="red"><i class="fa fa-phone-call"></i></span>
+        <span class="conversion__icon" data-color="max"><i class="fa fa-whatsapp"></i></span>
+        <span class="conversion__icon" data-color="blue"><i class="fa fa-email"></i></span>
       </div>
-      <div class="close" style="display: none;">
-        <span data-color="gray" style="display: inline;">
-          <i class="fa fa-close"></i>
-        </span>
+      <div class="conversion__close">
+        <span class="conversion__icon conversion__icon--close"><i class="fa fa-close"></i></span>
       </div>
-    </a>
+    </button>
   </div>
+
 </div>

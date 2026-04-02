0e52070fa091e84ef746ad695df862831591c6a7
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-02 16:45:46 +0300

    deploy

--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -1,16 +1,20 @@
 function initConversation() {
   const conversion = document.querySelector("[data-conversion]");
+  console.log(conversion);
 
   if (!conversion) return;
 
   const btn = conversion.querySelector("[data-conversion-btn]");
+  console.log(btn);
 
   if (!btn) return;
 
   const icons = btn.querySelectorAll("[data-conversion-btn-icon]") || [];
+  console.log(icons);
 
   if (icons.length > 0) {
     const iconSwitch = (index) => {
+      console.log(index);
       icons.forEach((el) => el.classList.remove("active"));
       icons[index].classList.add("active");
       const nextIndex = index == icons.length - 1 ? 0 : index + 1;

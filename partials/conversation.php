55d1faa9b700fe2145f1104e535cd73e5da6d9a9
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-02 16:40:35 +0300

    deploy

--- partials/conversation.php
+++ partials/conversation.php
@@ -1,4 +1,4 @@
-<div class="conversion conversion--widget">
+<div class="conversion conversion--widget" data-conversion>
   
   <!-- Контейнер виджета -->
   <div class="conversion__container">
@@ -127,18 +127,18 @@
   </div>
 
   <!-- Кнопка-триггер -->
-  <div class="conversion__btn conversion__btn--gray">
+  <button class="conversion__btn" data-conversion-btn type="button">
     <div class="conversion__btn-wave"></div>
-    <button class="conversion__toggler" type="button">
-      <div class="conversion__play">
-        <span class="conversion__icon" data-color="red"><i class="icon icon-phone"></i></span>
-        <span class="conversion__icon" data-color="max"><i class="icon icon-max"></i></span>
-        <span class="conversion__icon" data-color="blue"><i class="icon icon-email"></i></span>
+    <div class="conversion__btn-toggler">
+      <div class="conversion__btn-play">
+        <span class="conversion__btn-icon" data-conversion-btn-icon="red"><i class="icon icon-phone"></i></span>
+        <span class="conversion__btn-icon" data-conversion-btn-icon="max"><i class="icon icon-max"></i></span>
+        <span class="conversion__btn-icon" data-conversion-btn-icon="blue"><i class="icon icon-email"></i></span>
       </div>
-      <div class="conversion__close">
-        <span class="conversion__icon conversion__icon--close"><i class="icon icon-close"></i></span>
+      <div class="conversion__btn-close">
+        <span class="conversion__btn-icon conversion__btn-icon--close"><i class="icon icon-close"></i></span>
       </div>
-    </button>
-  </div>
+    </div>
+  </button>
 
 </div>
--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -1,5 +1,23 @@
 function initConversation() {
-  return null;
+  const conversion = document.querySelector("[data-conversion]");
+
+  if (!conversion) return;
+
+  const btn = conversion.querySelector("[data-conversion-btn]");
+
+  if (!btn) return;
+
+  const icons = btn.querySelectorAll("[data-conversion-btn-icon]") || [];
+
+  if (icons.length > 0) {
+    const iconSwitch = (index) => {
+      icons.forEach((el) => el.classList.remove("active"));
+      icons[index].classList.add("active");
+      const nextIndex = index == icons.length - 1 ? 0 : index + 1;
+      setTimeout(() => iconSwitch(nextIndex), 2000);
+    };
+    iconSwitch(0);
+  }
 }
 
 export { initConversation };

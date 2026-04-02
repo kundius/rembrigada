404e1896f506bb027e49b13538cd4ef8e76b7788
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-02 18:04:17 +0300

    deploy

--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -1,16 +1,13 @@
 function initConversation() {
   const conversion = document.querySelector("[data-conversion]");
-  console.log(conversion);
 
   if (!conversion) return;
 
   const btn = conversion.querySelector("[data-conversion-btn]");
-  console.log(btn);
 
   if (!btn) return;
 
   const icons = btn.querySelectorAll("[data-conversion-btn-icon]") || [];
-  console.log(icons);
 
   if (icons.length > 0) {
     const iconSwitch = (index) => {
@@ -22,11 +19,11 @@ function initConversation() {
         icons[index].classList.add("show-icon");
         setTimeout(() => {
           const nextIndex = index == icons.length - 1 ? 0 : index + 1;
-          setTimeout(() => iconSwitch(nextIndex), 10000);
+          setTimeout(() => iconSwitch(nextIndex), 2000);
         }, 250);
       }, 250);
     };
-    iconSwitch(icons.length - 1, 0);
+    iconSwitch(0);
   }
 }
 

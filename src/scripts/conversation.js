f958176f54a15e11d9647ea1dc66741604007602
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-02 17:35:40 +0300

    deploy

--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -21,7 +21,7 @@ function initConversation() {
           icons.forEach((el) => el.classList.remove("show-circle"));
           icons[index].classList.add("show-circle");
           const nextIndex = index == icons.length - 1 ? 0 : index + 1;
-          setTimeout(() => iconSwitch(nextIndex), 10000);
+          setTimeout(() => iconSwitch(nextIndex), 2000);
         }, 250);
       }, 250);
     };

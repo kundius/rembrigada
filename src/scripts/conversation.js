de8c0ca3cca76f602dbcecf9b14f44343a014a08
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-02 17:51:24 +0300

    deploy

--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -18,11 +18,11 @@ function initConversation() {
       setTimeout(() => {
         icons[index].classList.add("animate-first");
         icons.forEach((el) => el.classList.remove("animate-second"));
-        setTimeout(() => {
-          icons[index].classList.add("animate-second");
-          const nextIndex = index == icons.length - 1 ? 0 : index + 1;
-          setTimeout(() => iconSwitch(nextIndex), 10000);
-        }, 5000);
+        // setTimeout(() => {
+        icons[index].classList.add("animate-second");
+        const nextIndex = index == icons.length - 1 ? 0 : index + 1;
+        setTimeout(() => iconSwitch(nextIndex), 10000);
+        // }, 5000);
       }, 5000);
     };
     iconSwitch(icons.length - 1, 0);

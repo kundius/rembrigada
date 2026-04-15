e0de8c82c8395f4ec21859bd12dc5d96a29187a7
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-04-15 20:07:49 +0300

    123

--- partials/footer.php
+++ partials/footer.php
@@ -44,7 +44,7 @@
 <div id="callback" class="hidden">
     <div class="modal modal_callback">
         <button class="modal__close" data-basiclightbox-close></button>
-        <form action="/wp-json/contact-form-7/v1/contact-forms/379/feedback" method="post" class="callback js-form" data-ym-goal="CALLBACK_MODAL_FORM">
+        <form action="/wp-json/contact-form-7/v1/contact-forms/379/feedback" method="post" class="callback js-form" data-ym-goal="H_CALLBACK_FORM">
             <div class="callback__form">
                 <div class="callback__grid">
                     <div class="callback__cell">
